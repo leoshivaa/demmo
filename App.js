8af38a4734ae96bd585d7ade23830355e8dc06f3
leoshiva <146674606+leoshivaa@users.noreply.github.com>
2024-12-31 12:13:28 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hi this is shiva</h1>
+      </div>
+      <div>
+        <p>This is me shiva</p>
+        <p>This is a demo web page i want to check whether it will be deployed in the github or not and if so how it is done.</p>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
